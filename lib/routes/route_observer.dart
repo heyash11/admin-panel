3b67805994ac7912d2eb1a9553cbc3b7ebc3d763
heyash11 <48152034+heyash11@users.noreply.github.com>
2025-04-15 20:29:54 +0530

    Delete lib directory

--- lib/routes/route_observer.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:get/get_navigation/get_navigation.dart';
-
-class RouteObservers extends GetObserver {
-  @override
-  void didPop(Route<dynamic>? route, Route<dynamic>? previousRoute) {
-    // TODO: implement didPop
-  }
-}
